4f9e8da713557e573f31d0e805467b5f32da7389
BS <bs@bss-mac-pro.home>
2020-06-23 17:28:40 +0200

    working on orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,20 +1,40 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const Order = require("../models/orders");
 
 // Handle Orders requests
 router.get("/", (request, response, next) => {
-	response.status(200).json({
-		message: "Orders were Fetched",
-	});
+	Order.find()
+		.select("productId quantity")
+		.then((docs) => {
+			console.log(docs);
+			response.status(200).json(docs);
+		})
+		.catch((err) => {
+			console.log(err);
+			response.status(500).json({
+				error: err,
+			});
+		});
 });
 router.post("/", (request, response, next) => {
-	const order = {
-		productID: request.body.productID,
-		quantity: request.body.quantity,
-	};
-	response.status(201).json({
-		message: "Orders were Posted",
+	const order = new Order({
+		_id: mongoose.Types.ObjectId(),
+		quatity: request.body.quatity,
+		product: request.body.productId,
 	});
+	order
+		.save()
+		.then((result) => {
+			console.log(result);
+			response.status(201).json(result);
+		})
+		.catch((err) => {
+			console.log(err);
+			response.status(500).json({ error: err });
+		});
 });
 
 router.get("/:orderId", (request, response, next) => {
