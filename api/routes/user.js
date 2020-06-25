fe47877ec142df5ae746183e637681b6d9b0c859
BS <bs@bss-mac-pro.home>
2020-06-25 18:59:09 +0200

    working on signup

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,9 +1,63 @@
-const express = require('express')
-const router = express.Router()
-const mongoose = require('mongoose')
-const { route } = require('./orders')
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-router.post('/signup', (request, response, next) => {
+const User = require("../models/user");
 
-})
-module.exports = router
+router.post("/signup", (request, response, next) => {
+	User.find({ email: request.body.email })
+		.exec()
+		.then((user) => {
+			if (user.length >= 1) {
+				return response.status(409).json({
+					message: "email already exist !",
+				});
+			} else {
+				bcrypt.hash(request.body.password, 10, (err, hash) => {
+					if (err) {
+						return response.status(500).json({
+							err: err,
+						});
+					} else {
+						const user = new User({
+							_id: new mongoose.Types.ObjectId(),
+							email: request.body.email,
+							password: hash,
+						});
+						user
+							.save()
+							.then((result) => {
+								console.log(result);
+								response.status(201).json({
+									message: "User created",
+								});
+							})
+							.catch((err) => {
+								console.log(err);
+								response.status(500).json({
+									error: err,
+								});
+							});
+					}
+				});
+			}
+		});
+});
+
+router.delete("/:userId", (request, response, next) => {
+	User.remove({ _id: request.params.userId })
+		.exec()
+		.then((result) => {
+			response.status(200).json({
+				message: "User deleted",
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+			response.status(500).json({
+				error: err,
+			});
+		});
+});
+module.exports = router;
--- app.js
+++ app.js
@@ -1,65 +1,69 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
 // Middelware to handle logs
-const morgan = require('morgan')
-app.use(morgan('dev'))
+const morgan = require("morgan");
+app.use(morgan("dev"));
 
 // Connect the database
 mongoose.connect(
-	`mongodb+srv://bs:${process.env
-		.MONGO_PASS}@rest-api-bs-isfee.mongodb.net/rest-api-bs?retryWrites=true&w=majority`,
+	`mongodb+srv://bs:${process.env.MONGO_PASS}@rest-api-bs-isfee.mongodb.net/rest-api-bs?retryWrites=true&w=majority`,
 	{
 		// useMongoClient: true,
-		useNewUrlParser    : true,
-		useUnifiedTopology : true
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
 	}
-)
-mongoose.Promise = global.Promise
+);
+mongoose.Promise = global.Promise;
 
 // parse body data in json format
-app.use(express.urlencoded())
-app.use(express.json())
+app.use(express.urlencoded());
+app.use(express.json());
 
 // Make Static files publicly available
-app.use('/uploads', express.static('uploads'))
+app.use("/uploads", express.static("uploads"));
 
 // Handle CORS Access
 app.use((request, response, next) => {
-	response.header('Access-Control-Allow-Headers', '*')
+	response.header("Access-Control-Allow-Headers", "*");
 	response.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-	)
-	if (request.method === 'OPTIONS') {
-		response.header('Access-Control-Allow-Methods', 'PUT, POST, GET, PATCH, DELETE')
-		return response.status(200).json({})
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
+	if (request.method === "OPTIONS") {
+		response.header(
+			"Access-Control-Allow-Methods",
+			"PUT, POST, GET, PATCH, DELETE"
+		);
+		return response.status(200).json({});
 	}
-	next()
-})
+	next();
+});
 
-const productRoutes = require('./api/routes/products')
-const orderRoutes = require('./api/routes/orders')
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
+const userRoutes = require("./api/routes/user");
 
 // Link the differents routes to the application
-app.use('/products', productRoutes)
-app.use('/orders', orderRoutes)
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+app.use("/user", userRoutes);
 
 // Handle error
 app.use((request, response, next) => {
-	const error = new Error('Not Found')
-	error.status = 404
-	next(error)
-})
+	const error = new Error("Not Found");
+	error.status = 404;
+	next(error);
+});
 
 app.use((error, request, response, next) => {
-	response.status(error.status || 500)
+	response.status(error.status || 500);
 	response.json({
-		error : {
-			message : error.message
-		}
-	})
-})
+		error: {
+			message: error.message,
+		},
+	});
+});
 
-module.exports = app
+module.exports = app;
