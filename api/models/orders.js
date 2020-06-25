130928e74350312984f2dd8d7e44697e9e8dae94
BS <bs@macbook-pro-de-bs.home>
2020-06-25 12:41:42 +0200

    start the signup route

--- api/models/orders.js
+++ api/models/orders.js
@@ -1,14 +1,14 @@
-// Products mocks
-const mongoose = require("mongoose");
+// Orders mocks
+const mongoose = require('mongoose')
 
 const orderSchema = mongoose.Schema({
-	_id: mongoose.Schema.Types.ObjectId,
-	product: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "Product",
-		required: true,
+	_id      : mongoose.Schema.Types.ObjectId,
+	product  : {
+		type     : mongoose.Schema.Types.ObjectId,
+		ref      : 'Product',
+		required : true
 	},
-	quantity: { type: Number, default: 1 },
-});
+	quantity : { type: Number, default: 1 }
+})
 
-module.exports = mongoose.model("Order", orderSchema);
+module.exports = mongoose.model('Order', orderSchema)
