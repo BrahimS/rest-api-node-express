4ca51491ea6706a43ff748bd950af36b8eaa3a39
BS <bs@macbook-pro-de-bs.home>
2020-06-25 14:15:04 +0200

    start working on signup route

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const { route } = require('./orders')
 
+router.post('/signup', (request, response, next) => {
+
+})
 module.exports = router
